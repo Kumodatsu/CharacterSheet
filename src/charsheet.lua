7e3c673b4a55218b1671cef9840a6f3523798d28
Kumodatsu <dragonkumo@gmail.com>
2020-07-03 04:11:33 +0200

    Fix events not being handled

--- src/charsheet.lua
+++ src/charsheet.lua
@@ -88,12 +88,14 @@ end
 M.frame_load_vars:RegisterEvent("ADDON_LOADED")
 M.frame_load_vars:RegisterEvent("PLAYER_LOGOUT")
 
-M.frame_load_vars.OnEvent = function(event, arg1)
+M.frame_load_vars.OnEvent = function(self, event, arg1)
     if event == "ADDON_LOADED" and arg1 == addon_name then
         on_addon_loaded()
     elseif event == "PLAYER_LOGOUT" then
         on_addon_unloading()
     end
 end
+
+M.frame_load_vars:SetScript("OnEvent", M.frame_load_vars.OnEvent)
 
 cs.Charsheet = M
