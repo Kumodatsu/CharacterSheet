d7a3bed6bbb2e1604f20962b26772560b5755d31
Kumodatsu <dragonkumo@gmail.com>
2020-07-24 09:07:41 +0200

    Support TRP3 interoperation
    Add optional support for TRP3 interoperation to overwrite OOC information with one's stats.

---
+++ src/extensions/totalRP3.lua
@@ -0,0 +1,91 @@
+if not TRP3_API then return end
+
+local addon_name, cs = ...
+local M = {}
+
+local print = print
+
+-- Will be loaded from file on addon load
+M.UpdateTRPWithStats = false
+
+-- Set the player's OOC information to the given content string.
+-- The body of this function has been obtained by reverse engineering
+-- Tammya's TRP3 Currently Frame addon.
+local set_trp_ooc = function(content)
+    local character = TRP3_API.profile.getData("player/character")
+    local changed   = false
+    content = content or ""
+    if character.CO ~= content then
+        character.CO = content
+        changed      = true
+    end
+    if changed then
+        local context = TRP3_API.navigation.page.getCurrentContext()
+        if context and context.isPlayer then
+            TRP3_RegisterMiscViewCurrentlyOOCScrollText:SetText(
+                character.CO
+            )
+        end
+        character.v = TRP3_API.utils.math.incrementNumber(character.v or 1, 2)
+        TRP3_API.events.fireEvent( 
+            TRP3_API.events.REGISTER_DATA_UPDATED,
+            TRP3_API.globals.player_id, 
+            TRP3_API.profile.getPlayerCurrentProfileID(), 
+            "character"
+        )
+    end
+end
+
+local format_stats_string = function(hp, max_hp, str, dex, con, int, wis, cha)
+    return "HP: "  .. hp .. "/" .. max_hp ..
+        "\nSTR: "  .. str .. " / DEX: " .. dex .. " / CON: " .. con ..
+        " / INT: " .. int .. " / WIS: " .. wis .. " / CHA: " .. cha
+end
+
+local update_trp_stats = function()
+    if not M.UpdateTRPWithStats then
+        return
+    end
+    local stats = cs.Charsheet.Stats
+    set_trp_ooc(format_stats_string(
+        cs.Charsheet.CurrentHP,
+        stats:get_max_hp(),
+        stats.STR,
+        stats.DEX,
+        stats.CON,
+        stats.INT,
+        stats.WIS,
+        stats.CHA
+    ))
+end
+
+cs.Charsheet.OnStatsChanged:add(update_trp_stats)
+cs.Charsheet.OnCurrentHPChanged:add(update_trp_stats)
+
+TRP3_API.module.registerModule({
+    name        = GetAddOnMetadata(addon_name, "Title"),
+    description = GetAddOnMetadata(addon_name, "Notes"),
+    version     = tonumber(
+        GetAddOnMetadata(addon_name, "Version"):match("^%d+%.%d+")
+    ),
+    id          = "trp3_character_sheet",
+    onStart     = function() end,
+    onInit      = function() end,
+    minVersion  = 3
+});
+
+local toggle_stat_update = function()
+    M.UpdateTRPWithStats = not M.UpdateTRPWithStats
+    if M.UpdateTRPWithStats then
+        print("TRP OOC information now WILL be overwritten by your stats.")
+    else
+        print("TRP OOC information now WILL NOT be overwritten by your stats.")
+    end
+end
+
+cs.Commands.add_cmd("trp", toggle_stat_update, [[
+    "/cs trp" toggles whether your stats are allowed to overwrite your TRP's OOC information.
+]])
+
+cs.Extensions = cs.Extensions or {}
+cs.Extensions.totalRP3 = M
--- src/saving.lua
+++ src/saving.lua
@@ -11,12 +11,25 @@ local on_addon_loaded = function()
     cs.Charsheet.Stats     = cs.Stats.StatBlock.load(CS_Char_DB.Stats)
     cs.Charsheet.CurrentHP = CS_Char_DB.CurrentHP or cs.Charsheet.Stats:get_max_hp()
     cs.Charsheet.Pets      = CS_Char_DB.Pets      or {}
+
+    -- TRP settings
+    if cs.Extensions.totalRP3 then
+        cs.Extensions.totalRP3.UpdateTRPWithStats =
+            CS_Char_DB.TRP3_UpdateTRPWithStats or false
+    end
 end
 
 local on_addon_unloading = function()
+    -- Character stats
     CS_Char_DB.Stats     = cs.Charsheet.Stats:save()
     CS_Char_DB.CurrentHP = cs.Charsheet.CurrentHP
     CS_Char_DB.Pets      = cs.Charsheet.Pets
+
+    -- TRP settings
+    if cs.Extensions.totalRP3 then
+        CS_Char_DB.TRP3_UpdateTRPWithStats =
+            cs.Extensions.totalRP3.UpdateTRPWithStats
+    end
 end
 
 local frame_load_vars = CreateFrame("FRAME", "LoadData")
