a420a4762e213fca25b9a3aa053ff703dc1e720a
Kumodatsu <dragonkumo@gmail.com>
2020-07-24 09:01:08 +0200

    Add keybinds for incrementing and decrementing HP

--- src/main.lua
+++ src/main.lua
@@ -2,4 +2,8 @@ local addon_name, cs = ...
 
 CS_GetAddon = function()
     return cs
-end
+end
+
+BINDING_HEADER_CHARACTER_SHEET = "Character Sheet"
+BINDING_NAME_INCREMENT_HP      = "Increment HP"
+BINDING_NAME_DECREMENT_HP      = "Decrement HP"
