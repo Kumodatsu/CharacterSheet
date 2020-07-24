cf0448e20670213229a157a69261610f112e36f7
Kumodatsu <dragonkumo@gmail.com>
2020-07-24 19:57:43 +0200

    Register prefix for addon messages

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,19 @@
 local addon_name, cs = ...
 
+-- Global addon getter
 CS_GetAddon = function()
     return cs
 end
 
+-- Key bindings
 BINDING_HEADER_CHARACTER_SHEET = "Character Sheet"
 BINDING_NAME_INCREMENT_HP      = "Increment HP"
 BINDING_NAME_DECREMENT_HP      = "Decrement HP"
+
+-- Addon messages
+CS_MessagePrefix = "CS"
+local request_result =
+    C_ChatInfo.RegisterAddonMessagePrefix(CS_MessagePrefix)
+if not request_result then
+    message("The CharacterSheet addon could not register a message prefix. The addon may not work properly.")
+end
