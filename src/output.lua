28e4669c506ae179e99c173de6cf87527c42b30d
Kumodatsu <dragonkumo@gmail.com>
2020-07-29 01:25:13 +0200

    Fix print function

--- src/output.lua
+++ src/output.lua
@@ -2,7 +2,7 @@ local addon_name, cs = ...
 local M = {}
 
 M.Print = function(format, ...)
-    SendSystemMessage(format:format(...))
+    SendSystemMessage(string.format(format, ...))
 end
 
 cs.Output = M
