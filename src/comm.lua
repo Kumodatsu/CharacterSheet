1ac5b4dbb76fe8e6e7978eef8c7a365f88f5349f
Kumodatsu <dragonkumo@gmail.com>
2020-09-21 01:44:47 +0200

    Add functions for serializing stats

--- src/comm.lua
+++ src/comm.lua
@@ -23,4 +23,23 @@ CS.Commands.add_cmd("comm", send_addon_msg, [[
 "/cs comm <channel> <msg>" sends a CS addon message to the specified channel.
 ]])
 
+M.SerializeStats = function(stats)
+    stats = stats or CS.Charsheet.Stats
+    return string.format(
+        "%d:%d:%d:%d:%d:%d:%d",
+        stats.STR, stats.DEX, stats.CON,
+        stats.INT, stats.WIS, stats.CHA,
+        stats.Level
+    )
+end
+
+M.SerializeCharsheet = function(stats, hp)
+    hp = hp or CS.Charsheet.CurrentHP
+    return string.format(
+        "%s:%d",
+        M.SerializeStats(stats),
+        hp
+    )
+end
+
 CS.Comm = M
