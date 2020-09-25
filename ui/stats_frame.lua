ee7f8d20bd4add0608dc3ec49a93176a42bbadd8
Kumodatsu <dragonkumo@gmail.com>
2020-09-25 12:27:47 +0200

    Refactor stat frame code

--- ui/stats_frame.lua
+++ ui/stats_frame.lua
@@ -3,169 +3,178 @@ local addon_name, CS = ...
 -- Will be loaded from file on addon load
 CS.Interface.UIState.StatsFrameVisible = true
 
-local stat_icons = {
-    "Interface\\ICONS\\Icon_PetFamily_Beast.blp",
-    "Interface\\ICONS\\Icon_PetFamily_Flying.blp",
-    "Interface\\ICONS\\Icon_PetFamily_Humanoid.blp",
-    "Interface\\ICONS\\Icon_PetFamily_Mechanical.blp",
-    "Interface\\ICONS\\Icon_PetFamily_Dragon.blp",
-    "Interface\\ICONS\\Icon_PetFamily_Magical.blp"
-}
-local plusminus_icon = "Interface\\ACHIEVEMENTFRAME\\UI-ACHIEVEMENT-PLUSMINUS.BLP"
-local entry_width  = 110
-local entry_height = 32
-local entry_count  = #CS.Stats.AttributeNames
-
-local buttons = {}
-
-local stats_frame = CreateFrame("Frame", "CS_StatsFrame", UIParent)
-
-stats_frame:SetBackdrop {
-    bgFile   = "Interface\\DialogFrame\\UI-DialogBox-Background",
-    edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-    tile     = true,
-    tileSize = 32,
-    edgeSize = 32,
-    insets   = {
-        left   = 11,
-        right  = 12,
-        top    = 12,
-        bottom = 11
-    }
-}
-
-stats_frame:SetWidth(entry_width + 11 + 12)
-stats_frame:SetHeight(entry_count * entry_height + 11 + 12 + 20)
-stats_frame:SetPoint("CENTER", UIParent, "CENTER")
-stats_frame:EnableMouse(true)
-stats_frame:SetMovable(true)
-stats_frame:RegisterForDrag "LeftButton"
-stats_frame:SetScript("OnDragStart", function(self) self:StartMoving() end)
-stats_frame:SetScript("OnDragStop", function(self) self:StopMovingOrSizing() end)
-stats_frame:SetClampedToScreen(true)
-
-local health_bar = CreateFrame("StatusBar", nil, stats_frame)
-health_bar:SetPoint("TOP", stats_frame, "TOP", 0, -12)
-health_bar:SetOrientation "HORIZONTAL"
-health_bar:SetWidth(entry_width - 40)
-health_bar:SetHeight(20)
-health_bar:SetStatusBarTexture "Interface\\TARGETINGFRAME\\UI-StatusBar"
-health_bar:SetStatusBarColor(0.1, 0.9, 0.3, 1.0)
-
-health_bar.background = health_bar:CreateTexture(nil, "BACKGROUND")
-health_bar.background:SetTexture "Interface\\TARGETINGFRAME\\UI-StatusBar"
-health_bar.background:SetAllPoints(true)
-health_bar.background:SetVertexColor(0, 0.35, 0)
-
-health_bar.text = health_bar:CreateFontString(nil, "OVERLAY")
-health_bar.text:SetPoint("CENTER", health_bar, "CENTER", 0, 0)
-health_bar.text:SetFont("Fonts\\FRIZQT__.TTF", 16, "OUTLINE")
-health_bar.text:SetJustifyH "CENTER"
-health_bar.text:SetShadowOffset(1, -1)
-health_bar.text:SetTextColor(0.0, 1.0, 0.0)
-
-stats_frame.health_bar = health_bar
-
-local health_dec = CreateFrame("Button", nil, health_bar)
-health_dec:SetPoint("RIGHT", health_bar, "LEFT", 0, 0)
-health_dec:SetSize(entry_width, entry_height)
-health_dec.texture = health_dec:CreateTexture()
-health_dec.texture:SetPoint("RIGHT", health_bar, "LEFT", 0, 0)
-health_dec.texture:SetWidth(20)
-health_dec.texture:SetHeight(20)
-health_dec.texture:SetTexCoord(0.0, 0.5, 0.25, 0.5)
---health_dec.texture:SetTexCoord(0.0, 0.5, 0.0, 0.25)
-health_dec.texture:SetTexture(plusminus_icon)
-health_dec:SetScript("OnClick", function()
-    CS.Charsheet.set_hp(CS.Charsheet.CurrentHP - 1)
-end)
-
-local health_inc = CreateFrame("Button", nil, health_bar)
-health_inc:SetPoint("LEFT", health_bar, "RIGHT", 0, 0)
-health_inc:SetSize(entry_width, entry_height)
-health_inc.texture = health_inc:CreateTexture()
-health_inc.texture:SetPoint("LEFT", health_bar, "RIGHT", 0, 0)
-health_inc.texture:SetWidth(20)
-health_inc.texture:SetHeight(20)
-health_inc.texture:SetTexCoord(0.0, 0.5, 0.0, 0.25)
-health_inc.texture:SetTexture(plusminus_icon)
-health_inc:SetScript("OnClick", function()
-    CS.Charsheet.set_hp(CS.Charsheet.CurrentHP + 1)
-end)
-
-local create_button = function(i)
-    local button = CreateFrame("Button", nil, stats_frame)
-    if i == 1 then
-        button:SetPoint("TOP", stats_frame, "TOP", 0, -32)
-    else
-        button:SetPoint("TOP", buttons[i - 1], "BOTTOM")
-    end
-    button:SetNormalFontObject "GameFontNormal"
-    button:SetText "undefined"
-    button:GetFontString():SetPoint("LEFT", button, "LEFT", 32, 0)
-    button:GetFontString():SetPoint("RIGHT", button, "RIGHT", 0, 0)
-    button:SetSize(entry_width, entry_height)
-    button.texture = button:CreateTexture()
-    button.texture:SetPoint "TOPLEFT"
-    button.texture:SetWidth(32)
-    button.texture:SetHeight(32)
-    button.texture:SetTexture(stat_icons[i])
-    button:SetScript("OnClick", function()
-        CS.Charsheet.roll_stat(CS.Stats.AttributeNames[i])
-    end)
-    button:Show()
-    buttons[i] = button
-end
-
-for i = 1, entry_count do
-    create_button(i)
-end
-
-CS.Interface.update_hp_indicator = function()
+local update_hp_bar = function(self)
     local hp     = CS.Charsheet.CurrentHP
     local hp_max = CS.Charsheet.Stats:get_max_hp()
     local text   = string.format("%d/%d", hp, hp_max)
-    stats_frame.health_bar.text:SetText(text)
-    stats_frame.health_bar:SetMinMaxValues(0, hp_max)
-    stats_frame.health_bar:SetValue(hp)
+    self.text:SetText(text)
+    self:SetMinMaxValues(0, hp_max)
+    self:SetValue(hp)
 end
 
-CS.Interface.update_stats_buttons = function()
-    for i = 1, entry_count do
-        buttons[i]:SetText(string.format(
-            "%s: %d",
-            CS.Stats.AttributeNames[i],
-            CS.Charsheet.Stats[CS.Stats.AttributeNames[i]]
-        ))
+local update_stat_button = function(stat)
+    return function(self)
+        local text = string.format("%s: %d", stat, CS.Charsheet.Stats[stat])
+        self:SetText(text)
     end
 end
 
-CS.Charsheet.OnStatsChanged
-    :add(CS.Interface.update_stats_buttons)
-CS.Charsheet.OnHPChanged
-    :add(CS.Interface.update_hp_indicator)
-CS.OnAddonLoaded
-    :add(CS.Interface.update_hp_indicator)
-    :add(CS.Interface.update_stats_buttons)
-    :add(function()
-        if CS.Interface.UIState.StatsFrameVisible then
-            stats_frame:Show()
-        else
-            stats_frame:Hide()
-        end
-    end)
-CS.OnAddonUnloading
-    :add(function()
-        CS.Interface.UIState.StatsFrameVisible = stats_frame:IsVisible()
-    end)
-
-CS.Interface.ToggleStatsFrame = function()
-    if stats_frame:IsVisible() then
-        stats_frame:Hide()
-    else
-        stats_frame:Show()
-    end
-end
+CS.Interface.Frame {
+    Global     = "CS_StatsFrame",
+    Backdrop   = {
+        Background = "Interface\\DialogFrame\\UI-DialogBox-Background",
+        Edges      = "Interface\\DialogFrame\\UI-DialogBox-Border",
+        Tiled      = true,
+        TileSize   = 32,
+        EdgeSize   = 32,
+        Insets     = {
+            Left   = 11,
+            Right  = 12,
+            Top    = 12,
+            Bottom = 11
+        }
+    },
+    Width      = 110,
+    Height     = 20 + 6 * 32,
+    Point      = { "CENTER", UIParent, "CENTER" },
+    Movable    = true,
+    Clamped    = true,
+    Events     = {
+        [CS.OnAddonLoaded]    = { function(self)
+            if CS.Interface.UIState.StatsFrameVisible then
+                self:Show()
+            else
+                self:Hide()
+            end
+        end },
+        [CS.OnAddonUnloading] = { function(self)
+            CS.Interface.UIState.StatsFrameVisible = self:IsVisible()
+        end }
+    },
+    Content    = {
+        CS.Interface.Button {
+            Width     = 20,
+            Height    = 20,
+            Texture   = "Interface\\ACHIEVEMENTFRAME\\UI-ACHIEVEMENT-PLUSMINUS.BLP",
+            TexCoords = { 0.0, 0.5, 0.25, 0.5 },
+            OnClick   = CS.Charsheet.decrement_hp
+        },
+        CS.Interface.StatusBar {
+            Orientation = "HORIZONTAL",
+            Width       = 70,
+            Height      = 20,
+            Foreground = {
+                Texture = "Interface\\TARGETINGFRAME\\UI-StatusBar",
+                Color   = { 0.1, 0.9, 0.3, 1.0 }
+            },
+            Background  = {
+                Texture = "Interface\\TARGETINGFRAME\\UI-StatusBar",
+                Color   = { 0.0, 0.35, 0.0, 1.0 }
+            },
+            Events      = {
+                [CS.OnAddonLoaded]         = { update_hp_bar },
+                [CS.Charsheet.OnHPChanged] = { update_hp_bar }
+            }
+        },
+        CS.Interface.Button {
+            Width     = 20,
+            Height    = 20,
+            Texture   = "Interface\\ACHIEVEMENTFRAME\\UI-ACHIEVEMENT-PLUSMINUS.BLP",
+            TexCoords = { 0.0, 0.5, 0.0, 0.25 },
+            OnClick   = CS.Charsheet.increment_hp
+        },
+        CS.Interface.Icon {
+            Width   = 32,
+            Height  = 32,
+            Texture = "Interface\\ICONS\\Icon_PetFamily_Beast.blp"
+        },
+        CS.Interface.Button {
+            Width   = 110 - 32,
+            Height  = 32,
+            Text    = "STR",
+            OnClick = CS.fwd(CS.Charsheet.roll_stat, "STR"),
+            Events  = {
+                [CS.OnAddonLoaded]            = { update_stat_button "STR" },
+                [CS.Charsheet.OnStatsChanged] = { update_stat_button "STR" }
+            }
+        },
+        CS.Interface.Icon {
+            Width   = 32,
+            Height  = 32,
+            Texture = "Interface\\ICONS\\Icon_PetFamily_Flying.blp"
+        },
+        CS.Interface.Button {
+            Width   = 110 - 32,
+            Height  = 32,
+            Text    = "DEX",
+            OnClick = CS.fwd(CS.Charsheet.roll_stat, "DEX"),
+            Events  = {
+                [CS.OnAddonLoaded]            = { update_stat_button "DEX" },
+                [CS.Charsheet.OnStatsChanged] = { update_stat_button "DEX" }
+            }
+        },
+        CS.Interface.Icon {
+            Width   = 32,
+            Height  = 32,
+            Texture = "Interface\\ICONS\\Icon_PetFamily_Humanoid.blp"
+        },
+        CS.Interface.Button {
+            Width   = 110 - 32,
+            Height  = 32,
+            Text    = "CON",
+            OnClick = CS.fwd(CS.Charsheet.roll_stat, "CON"),
+            Events  = {
+                [CS.OnAddonLoaded]            = { update_stat_button "CON" },
+                [CS.Charsheet.OnStatsChanged] = { update_stat_button "CON" }
+            }
+        },
+        CS.Interface.Icon {
+            Width   = 32,
+            Height  = 32,
+            Texture = "Interface\\ICONS\\Icon_PetFamily_Mechanical.blp"
+        },
+        CS.Interface.Button {
+            Width   = 110 - 32,
+            Height  = 32,
+            Text    = "INT",
+            OnClick = CS.fwd(CS.Charsheet.roll_stat, "INT"),
+            Events  = {
+                [CS.OnAddonLoaded]            = { update_stat_button "INT" },
+                [CS.Charsheet.OnStatsChanged] = { update_stat_button "INT" }
+            }
+        },
+        CS.Interface.Icon {
+            Width   = 32,
+            Height  = 32,
+            Texture = "Interface\\ICONS\\Icon_PetFamily_Dragon.blp"
+        },
+        CS.Interface.Button {
+            Width   = 110 - 32,
+            Height  = 32,
+            Text    = "WIS",
+            OnClick = CS.fwd(CS.Charsheet.roll_stat, "WIS"),
+            Events  = {
+                [CS.OnAddonLoaded]            = { update_stat_button "WIS" },
+                [CS.Charsheet.OnStatsChanged] = { update_stat_button "WIS" }
+            }
+        },
+        CS.Interface.Icon {
+            Width   = 32,
+            Height  = 32,
+            Texture = "Interface\\ICONS\\Icon_PetFamily_Magical.blp"
+        },
+        CS.Interface.Button {
+            Width   = 110 - 32,
+            Height  = 32,
+            Text    = "CHA",
+            OnClick = CS.fwd(CS.Charsheet.roll_stat, "CHA"),
+            Events  = {
+                [CS.OnAddonLoaded]            = { update_stat_button "CHA" },
+                [CS.Charsheet.OnStatsChanged] = { update_stat_button "CHA" }
+            }
+        }
+    }
+}
 
 local toggle_frame = function(name)
     if not name then
@@ -173,7 +182,7 @@ local toggle_frame = function(name)
     end
     name = name:lower()
     if name == "stats" then
-        CS.Interface.ToggleStatsFrame()
+        CS.Interface.Toggle(CS_StatsFrame)
     elseif name == "tabs" then
         CS.Interface.ToggleMainFrame()
     elseif name == "edit" then
