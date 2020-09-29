a97cd0181d27cd75e0985ca62abaeff7e4016dcf
Kumodatsu <dragonkumo@gmail.com>
2020-09-29 19:31:08 +0200

    Add half function

--- src/util/math.lua
+++ src/util/math.lua
@@ -5,4 +5,17 @@ M.round = function(x)
     return math.floor(x + 0.5)
 end
 
+M.half = function(x, mode)
+    x = tonumber(x)
+    mode = mode or "up"
+    mode = mode:lower()
+    local half_x = x / 2
+    if mode == "up" then
+        return math.ceil(half_x)
+    elseif mode == "down" then
+        return math.floor(half_x)
+    end
+    return nil
+end
+
 CS.Math = M
