8af2218aa8d5c985f0780edda58a6994516dbeb0
Kumodatsu <dragonkumo@gmail.com>
2020-10-11 11:11:49 +0200

    Add new table util functions

--- src/util/table.lua
+++ src/util/table.lua
@@ -21,4 +21,49 @@ M.is_empty = function(t)
     return true
 end
 
+M.get_keys = function(t)
+    local ks = {}
+    for k, _ in pairs(t) do
+        table.insert(ks, k)
+    end
+    return ks
+end
+
+M.get_sorted_keys = function(t, sort)
+    local ks = M.get_keys(t)
+    table.sort(ks, sort)
+    return ks
+end
+
+M.nearest_sorted = function(ks, v)
+    local n = nil
+    for i = 1, #ks do
+        local k = ks[i]
+        if v < k then
+            return n
+        end
+        n = k
+    end
+    return n
+end
+
+M.nearest = function(ks, v)
+    local r = { unpack(ks) }
+    table.sort(r)
+    return M.nearest_sorted(r, v)
+end
+
+M.map = function(t, f)
+    local r = {}
+    for k, v in pairs(t) do
+        r[k] = f(v)
+    end
+    return r
+end
+
+M.metatable = function(t)
+    setmetatable(t, t)
+    return t
+end
+
 CS.Table = M
