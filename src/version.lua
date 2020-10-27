9b9b888df0508158f2c70a5477f500828e97bd76
Kumodatsu <dragonkumo@gmail.com>
2020-10-27 07:23:29 +0100

    Add string version comparison function

--- src/version.lua
+++ src/version.lua
@@ -47,4 +47,16 @@ M.from_str = function(str)
     }
 end
 
+M.compare = function(a, b)
+    local a_major, a_minor, a_patch = CS.String.match(a, "%d+")
+    local b_major, b_minor, b_patch = CS.String.match(b, "%d+")
+    if a_major > b_major then return  1 end
+    if a_major < b_major then return -1 end
+    if a_minor > b_minor then return  1 end
+    if a_minor < b_minor then return -1 end
+    if a_patch > b_patch then return  1 end
+    if a_patch < b_patch then return -1 end
+    return 0
+end
+
 CS.Version = M
