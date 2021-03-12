ef3cef05fe0f251d4386d48ade72277237a3df66
Kumodatsu <dragonkumo@gmail.com>
2021-03-12 16:43:52 +0100

    Add match and compare_versions functions

--- CharacterSheet/src/Core/Util.lua
+++ CharacterSheet/src/Core/Util.lua
@@ -3,6 +3,14 @@ CS.Util = {}
 
 local M = CS.Util
 
+M.match = function(str, pattern)
+    local t = {}
+    for m in str:gmatch(pattern) do
+        table.insert(t, m)
+    end
+    return unpack(t)
+end
+
 M.iformat = function(format, ...)
     local args, order = { ... }, {}
     format = format:gsub("%%(%d+)%$", function(i)
@@ -14,4 +22,16 @@ end
 
 M.multiline = function(str)
     return str:gsub("^%s*", ""):gsub("\n%s*", "\n")
+end
+
+M.compare_versions = function(a, b)
+    local a_major, a_minor, a_patch = M.match(a, "%d+")
+    local b_major, b_minor, b_patch = M.match(b, "%d+")
+    if a_major > b_major then return  1 end
+    if a_major < b_major then return -1 end
+    if a_minor > b_minor then return  1 end
+    if a_minor < b_minor then return -1 end
+    if a_patch > b_patch then return  1 end
+    if a_patch < b_patch then return -1 end
+    return 0
 end
