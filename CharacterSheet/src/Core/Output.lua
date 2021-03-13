bb64506462802884dab00678178a620d69312b5c
Kumodatsu <dragonkumo@gmail.com>
2021-03-13 20:05:50 +0100

    Document the Output module

--- CharacterSheet/src/Core/Output.lua
+++ CharacterSheet/src/Core/Output.lua
@@ -1,5 +1,14 @@
+--- Functions for presenting output.
+-- @module CS.Output
+
 local addon_name, CS = ...
 
+--[[--
+    Prints a formatted string to the chat as a system message.
+    This function formats strings in the same way as string.format.
+    @tparam string format The format string.
+    @param[optchain] ... The values to format the string with.
+]]
 CS.print = function(format, ...)
     SendSystemMessage(string.format(format, ...))
 end
