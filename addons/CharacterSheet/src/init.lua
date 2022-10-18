12499d98f1b5e3dd8a1c364c0e9760ace20b2db0
Kumodatsu <dragonkumo@gmail.com>
2022-10-18 14:30:41 +0200

    Remove addon loaded message

--- addons/CharacterSheet/src/init.lua
+++ addons/CharacterSheet/src/init.lua
@@ -1,7 +1,7 @@
 local addon_name, CS = ...
 
 CS.Core.Event.subscribe_event("WoW.AddonLoaded", function(...)
-  print "Addon loaded!"
+  
 end)
 
 -- Global accessor to the addon's public functionality.
