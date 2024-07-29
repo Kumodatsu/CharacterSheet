206f8aeb1ac349823ba36b53b9fda53bf149b9c8
Kumodatsu <dragonkumo@gmail.com>
2024-07-29 19:10:26 +0200

    Update addon compartment menu for 11.0.0

--- addons/CharacterSheet/ui/compartment.lua
+++ addons/CharacterSheet/ui/compartment.lua
@@ -2,63 +2,50 @@ local addon_name, CS = ...
 
 local T = CS.Locale.GetLocaleTranslations()
 
-local menu = {
-  {
-    text         = "Options",
-    notCheckable = true,
-    func         = function()
-      local title = GetAddOnMetadata(addon_name, "title")
-      -- Have to call this function twice because of a WoW API-side bug.
-      InterfaceOptionsFrame_OpenToCategory(title)
-      InterfaceOptionsFrame_OpenToCategory(title)
-    end,
-  },
-  {
-    text         = T.MINIMAP_MENU_UI_FRAMES,
-    isTitle      = true,
-    notCheckable = true,
-  },
-  {
-    text = T.MINIMAP_MENU_STATS_FRAME,
-    func = function()
-      CS.Interface.Toggle(CS_StatsFrame)
-    end,
-    checked = function()
-      return CS_StatsFrame:IsVisible()
-    end,
-  },
-  {
-    text = T.MINIMAP_MENU_EDIT_FRAME,
-    func = function()
-      CS.Interface.Toggle(CS_EditFrame)
-    end,
-    checked = function()
-      return CS_EditFrame:IsVisible()
-    end,
-  },
-  {
-    text = T.MINIMAP_MENU_TOGGLE_PET,
-    func = function()
-      CS.Mechanics.Sheet:toggle_pet()
-    end,
-    checked = function()
-      return CS.Mechanics.Sheet.PetActive
-    end,
-  },
-  {
-    text = T.MINIMAP_MENU_RESOURCE,
-    func = function()
-      CS.Interface.ResourceMenu.open()
-    end,
-    notCheckable = true,
-  },
-}
-
 AddonCompartmentFrame:RegisterAddon {
   text         = "Character Sheet",
   icon         = "Interface/Icons/inv_inscription_scroll",
   notCheckable = true,
   func         = function()
-    EasyMenu(menu, CreateFrame "Frame", "cursor", 0, 0)
+    MenuUtil.CreateContextMenu(nil, function(parent, root)
+      root:CreateTitle "Character Sheet"
+      root:CreateButton("Options", function()
+        local title = GetAddOnMetadata(addon_name, "title")
+        -- Have to call this function twice because of a WoW API-side bug.
+        InterfaceOptionsFrame_OpenToCategory(title)
+        InterfaceOptionsFrame_OpenToCategory(title)
+      end)
+      root:CreateTitle(T.MINIMAP_MENU_UI_FRAMES)
+      root:CreateCheckbox(
+        T.MINIMAP_MENU_STATS_FRAME,
+        function()
+          return CS_StatsFrame:IsVisible()
+        end,
+        function()
+          CS.Interface.Toggle(CS_StatsFrame)
+        end
+      )
+      root:CreateCheckbox(
+        T.MINIMAP_MENU_EDIT_FRAME,
+        function()
+          return CS_EditFrame:IsVisible()
+        end,
+        function()
+          CS.Interface.Toggle(CS_EditFrame)
+        end
+      )
+      root:CreateCheckbox(
+        T.MINIMAP_MENU_TOGGLE_PET,
+        function()
+          return CS.Mechanics.Sheet.PetActive
+        end,
+        function()
+          CS.Mechanics.Sheet:toggle_pet()
+        end
+      )
+      root:CreateButton(T.MINIMAP_MENU_RESOURCE, function()
+        CS.Interface.ResourceMenu.open()
+      end)
+    end)
   end,
 }
